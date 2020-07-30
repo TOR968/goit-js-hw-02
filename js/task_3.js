ee64d9b4a656ae4a99638775a8a8a1ba6779ceb3
Maxim <Tor968@yahoo.com>
2020-07-30 09:44:34 +0300

    Create task_3.js

---
+++ js/task_3.js
@@ -0,0 +1,12 @@
+function findLongestWord(string = '') {
+  // Write code under this line
+}
+
+//console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+//console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+//console.log(findLongestWord('May the force be with you'));
+// 'force'
