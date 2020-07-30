5b37ef941164f5fb4f873817764bcc9a432b708b
Maxim <Tor968@yahoo.com>
2020-07-30 14:29:28 +0300

    Update task_3.js

--- js/task_3.js
+++ js/task_3.js
@@ -1,12 +1,20 @@
 function findLongestWord(string = '') {
   // Write code under this line
+  let longWord = '';
+  let word = '';
+  for (let i = 0; i < string.split(' ').length; i += 1) {
+    if (string.split(' ')[i].length > longWord) {
+      longWord = string.split(' ')[i].length;
+      word = string.split(' ')[i];
+    }
+  }
+  return word;
 }
-
-//console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
 // 'jumped'
 
-//console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('Google do a roll'));
 // 'Google'
 
-//console.log(findLongestWord('May the force be with you'));
+console.log(findLongestWord('May the force be with you'));
 // 'force'
