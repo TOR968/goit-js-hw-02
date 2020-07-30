f4ee1405924fe9c2b7271701455a46a82195608f
Maxim <Tor968@yahoo.com>
2020-07-30 16:54:40 +0300

    Update task_4.js

--- js/task_4.js
+++ js/task_4.js
@@ -1,12 +1,21 @@
 function formatString(string, maxLength = 40) {
   // Write code under this line
+  let removed = '';
+  let result = '';
+  if (string.split('').length > maxLength) {
+    return string.split('').splice(maxLength, 100, '...');
+  } else {
+    return string;
+  }
+  //   return removed;
 }
-
-//console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 // 'Curabitur ligula sapien, tincidunt non.'
 
-//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
 // 'Vestibulum facilisis, purus nec pulvinar...'
 
-//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+console.log(
+  formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30),
+);
 // 'Vestibulum facilisis, purus ne...'
