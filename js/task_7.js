18ed2779ee3eb7e7cb06c2c1fd299e98adba71f2
Maxim Turbal <tor968@yahoo.com>
2020-08-01 19:44:19 +0300

    Update task_7.js

--- js/task_7.js
+++ js/task_7.js
@@ -1,29 +1,16 @@
 function filterArray(array) {
   const numbers = [];
-  // let result = '';
+
   for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
-
     const typeArr = typeof array[i];
-    if (typeArr === 'number' && typeArr !== 'NaN' && typeArr !== 'Infinity') {
-      const onlyNumber = array[i];
-      console.log(onlyNumber);
+    if (typeArr === 'number' && Number.isFinite(array[i])) {
+      numbers.push(array[i]);
     }
-
-    // if (typeArr.includes('number')) {
-    //   console.log(array[i]);
-    // }
-
-    // console.log(array[i]);
-    // console.log(typeof array[i]);
-    // === 'number'
-    // ? console.log(array)
-    // : console.log(``);
   }
-  // return result;
   return numbers;
 }
-
+// && isNaN(typeArr)
 console.log(filterArray([-2, 0, 2]));
 // [-2, 0, 2]
 
